1ff2faef306e975efe54604d9e6455801147a456
sunil-polupalli <polupallisunil999@gmail.com>
2026-01-19 11:00:22 +0530

    Updated frontend pages

--- backend/src/main/java/com/gateway/controllers/TestController.java
+++ backend/src/main/java/com/gateway/controllers/TestController.java
@@ -3,6 +3,7 @@ package com.gateway.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/test")
+@CrossOrigin
 public class TestController {
 
     @Autowired private RedisTemplate<String, Object> redisTemplate;
